7243f7913174addf6bb3cd56bf0aa7dcd75cfdc4
DuDu <ruslan_cek@mail.ru>
2019-12-25 07:50:47 +0300

    настройка подключения к базе

--- src/sample/controllers/settingsConnectController.java
+++ src/sample/controllers/settingsConnectController.java
@@ -1,4 +1,25 @@
 package sample.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class settingsConnectController {
+
+    @FXML
+    private TextField dbPass_Field;
+
+    @FXML
+    private PasswordField dbName_Field;
+
+    @FXML
+    private TextField dbHost_Field;
+
+    @FXML
+    private TextField dbUser_Field;
+
+    @FXML
+    private Button testConnectButton;
+
 }
